e129828ab7e4ec98cc5ddca6dc5caf1645e2ede4
enezihe <enezihe@gmail.com>
2026-03-15 15:25:49 -0400

    Add Lab 7 SwiftUI project

---
+++ Lab7NavigationApp/Lab7NavigationApp/Lab7NavigationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab7NavigationAppApp.swift
+//  Lab7NavigationApp
+//
+//  Created by nbt on 2026-03-15.
+//
+
+import SwiftUI
+
+@main
+struct Lab7NavigationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
